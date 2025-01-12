572c2cf41d4a80fc50bcb55ae08a75f641deed11
Ulyana <158500343+Ulyana-Tykhovskaya@users.noreply.github.com>
2025-01-12 14:51:29 +0100

    Update

--- js/task-07.js
+++ js/task-07.js
@@ -1,3 +1,12 @@
 // Напишіть функцію savePassword(password), яка приймає пароль та повертає внутрішню функцію,
 //     яка приймає рядок та повертає true, якщо рядок співпадає зі збереженим паролем,
 //     та false - якщо не співпадає
+
+function savePassword(password) {
+  return function (str) {
+    return password === str;
+  };
+}
+const userPassword = savePassword(12345);
+console.log(userPassword(135));
+console.log(userPassword(12345))
